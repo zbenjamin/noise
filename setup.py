7557d5f02c0be3e13fb58553dea84a4a75251d2c
Zev Benjamin <zev@strangersgate.com>
2017-05-08 22:18:14 -0700

    Bump version and update changelog

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 
 setup(
     name='vec_noise',
-    version='1.1.2',
+    version='1.1.3',
     description='Vectorized Perlin noise for Python',
     long_description='''\
 This is a fork of Casey Duncan's noise library that vectorizes all of the noise
