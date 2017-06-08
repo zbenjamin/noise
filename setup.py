58fce8794c0fa9c78d3f3e34268645aa449cfec4
Zev Benjamin <zev@strangersgate.com>
2017-06-08 16:02:18 -0700

    Add build-time dependency on numpy

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 import sys
-try:
-    from setuptools import setup, Extension
-except ImportError:
-    from distutils.core import setup, Extension
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 if sys.platform != 'win32':
     compile_args = ['-funroll-loops']
@@ -51,6 +58,9 @@ for convenient generation of turbulent noise are also included.
 
     package_dir={'vec_noise': ''},
     packages=['vec_noise'],
+    setup_requires=['numpy'],
+    install_requires=['numpy'],
+    cmdclass={'build_ext': build_ext},
     ext_modules=[
         Extension('vec_noise._simplex', ['_simplex.c'],
             extra_compile_args=compile_args,
